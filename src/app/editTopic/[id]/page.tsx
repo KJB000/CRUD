ba5a737a6e563cbd6cf459f74067e2fe29c296ee
KJB000 <smcha524@naver.com>
2024-11-14 17:03:48 +0900

    Update page.tsx

--- src/app/editTopic/[id]/page.tsx
+++ src/app/editTopic/[id]/page.tsx
@@ -1,36 +1,29 @@
-import EditTopicForm from '@/components/EditTopicForm';
+import EditTopicForm from '@/components/EditTopicForm'
+import React from 'react'
 
-const apiUrl = process.env.API_URL;
+const apiUrl = process.env.API_URL
 
 const getTopicById = async (id: string) => {
   try {
     const res = await fetch(`${apiUrl}/api/topics/${id}`, {
       cache: 'no-store',
-    });
+    })
     if (!res.ok) {
-      throw new Error('Failed to fetch topic.');
+      throw new Error('Failed to fetch topic')
     }
-    const data = await res.json();
-    return data && data.topic ? { topic: data.topic } : { topic: null }; // 항상 { topic: ... } 형태로 반환
+    return res.json()
   } catch (error) {
-    console.log(error);
-    return { topic: null }; // 오류 발생 시 기본값 반환
+    console.log(error)
   }
-};
+}
 
-export default async function EditTopic({
+export default async function EditTopicPage({
   params,
 }: {
   params: { id: string }
 }) {
-  const { id } = params;
-  const data = await getTopicById(id); // getTopicById 호출 후 변수에 저장
-
-  if (!data || !data.topic) {
-    // 데이터가 없거나 topic이 null일 경우 처리
-    return <div>Failed to load topic. Please try again later.</div>;
-  }
-
-  const { title, description } = data.topic;
-  return <EditTopicForm id={id} title={title} description={description} />;
+  const { id } = await params
+  const { topic } = await getTopicById(id)
+  const { title, description } = topic
+  return <EditTopicForm id={id} title={title} description={description} />
 }
