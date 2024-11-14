a5154f850af95e44fd53febacbd52fa3ed59a98f
KJB000 <smcha524@naver.com>
2024-11-14 16:56:37 +0900

    Update page.tsx

--- src/app/editTopic/[id]/page.tsx
+++ src/app/editTopic/[id]/page.tsx
@@ -1,25 +1,34 @@
-import EditTopicForm from '@/components/EditTopicForm'
-const apiUrl = process.env.API_URL
+import EditTopicForm from '@/components/EditTopicForm';
+
+const apiUrl = process.env.API_URL;
+
 const getTopicById = async (id: string) => {
   try {
     const res = await fetch(`${apiUrl}/api/topics/${id}`, {
       cache: 'no-store',
-    })
+    });
     if (!res.ok) {
-      throw new Error('Failed to fetch topic.')
+      throw new Error('Failed to fetch topic.');
     }
-    return res.json()
+    return await res.json();
   } catch (error) {
-    console.log(error)
+    console.log(error);
+    return { topic: null }; // 오류 발생 시 기본값 반환
   }
-}
+};
+
 export default async function EditTopic({
   params,
 }: {
   params: { id: string }
 }) {
-  const { id } = await params
-  const { topic } = await getTopicById(id)
-  const { title, description } = topic
-  return <EditTopicForm id={id} title={title} description={description} />
+  const { id } = params;
+  const { topic } = await getTopicById(id);
+
+  if (!topic) {
+    return <div>Failed to load topic. Please try again later.</div>;
+  }
+
+  const { title, description } = topic;
+  return <EditTopicForm id={id} title={title} description={description} />;
 }
