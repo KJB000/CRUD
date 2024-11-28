b7cd522439c56b8332e86db54b96b51238046bc5
KJB000 <smcha524@naver.com>
2024-11-28 17:47:22 +0900

    new

--- src/app/editTopic/[id]/page.tsx
+++ src/app/editTopic/[id]/page.tsx
@@ -1,33 +1,24 @@
 import { auth } from '@/auth'
 import EditTopicForm from '@/components/EditTopicForm'
-<<<<<<< HEAD
 import { redirect } from 'next/navigation'
-=======
->>>>>>> ba5a737a6e563cbd6cf459f74067e2fe29c296ee
-import React from 'react'
 
 const apiUrl = process.env.API_URL
 
-<<<<<<< HEAD
-async function getTopicById(id: string) {
-=======
 const getTopicById = async (id: string) => {
->>>>>>> ba5a737a6e563cbd6cf459f74067e2fe29c296ee
   try {
     const res = await fetch(`${apiUrl}/api/topics/${id}`, {
       cache: 'no-store',
     })
     if (!res.ok) {
-      throw new Error('Failed to fetch topic')
+      throw new Error('Failed to fetch topic.')
     }
     return res.json()
   } catch (error) {
-    console.error('Error fetching topic:', error)
-    return null
+    console.log(error)
   }
 }
 
-export default async function EditTopicPage({
+export default async function EditTopic({
   params,
 }: {
   params: { id: string }
@@ -37,14 +28,10 @@ export default async function EditTopicPage({
     redirect('/login')
   }
 
-  const { id } = params
-  const data = await getTopicById(id)
-
-  if (!data || !data.topic) {
-    return <p>Topic not found</p>
-  }
-
-  const { title, description } = data.topic
+  const { id } = await params
+  const { topic } = await getTopicById(id)
+  // console.log(topic)
+  const { title, description } = topic
 
   return <EditTopicForm id={id} title={title} description={description} />
 }
